aa3e1274e88da1805e101dd979279b3fbda393d1
Yosef Mostafa <s-yosef_mostafa@zewailcity.edu.eg>
2025-09-23 07:21:06 +0300

    Navigated throught the angular platform
    -- Added first componet with general styles also imported images from assets

---
+++ src/app/header/header.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true, // if set to false to will be module based component
+  templateUrl: './header.html',
+  styleUrls: ['./header.css']
+})
+export class HeaderComponent {
+
+}
